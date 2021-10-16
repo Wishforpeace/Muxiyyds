fe43b722963c12365491dfa1881295e3a31441e4
wishforpeace <1903180340@qq.com>
2021-10-16 15:20:14 +0800

    printgraphics

--- printgraphics.c
+++ printgraphics.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 int main(){
 	int i,j,k;
-	for(i=1;i<=3;i++){
-		if(i==2){
-			continue;
-		}
+	for(i=0;i<=2;i++){
+		
 		for(j=0;j<i;j++){
+		printf(" ");
+		}
+		for(j=0;j<5-2*i;j++){
+		printf("*");
+		}
+	printf("\n");
+	}
+	for(i=0;i<2;i++){
+		for(j=1;j>i;j--){
 		printf(" ");
 		
 		}
-		for(j=0;j<5-2*i;j++){
-			printf("*");
-			
-		}
+		for(j=1;j<=2*i+3;j++){
+		printf("*");
+		}	
 		printf("\n");
-	};
-	
+	}
 	return 0;
 }
